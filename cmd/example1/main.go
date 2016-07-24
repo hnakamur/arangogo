38756f5fce2ec5882382490f6b38c7d644553f06
Hiroaki Nakamura <hnakamur@gmail.com>
2016-07-24 12:18:01 +0900

    Initial commit

---
+++ cmd/example1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hnakamur/arangogo"
+)
+
+func main() {
+	err := run("root", "root")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run(username, password string) error {
+	c, err := arangogo.NewConnection(&arangogo.Config{Username: username, Password: password})
+	if err != nil {
+		return err
+	}
+
+	name := "foo"
+	err = c.CreateDatabase(name, []interface{}{
+		map[string]interface{}{
+			"username": "root",
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	databases, err := c.ListDatabases()
+	if err != nil {
+		return err
+	}
+	log.Printf("databases=%v", databases)
+
+	err = c.DropDatabase(name)
+	if err != nil {
+		return err
+	}
+
+	userDatabases, err := c.ListUserDatabases()
+	if err != nil {
+		return err
+	}
+	log.Printf("userDatabases=%v", userDatabases)
+	return nil
+}
